8f06441928dfb6a282f763b280d4dc81c6272772
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-05-01 01:28:45 +0300

    pics move to public

--- src/components/BackgroundImage/BackgroundImage.styled.jsx
+++ src/components/BackgroundImage/BackgroundImage.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 // prettier-ignore
 export const BackgroundImageRandom = styled.div`
-  background-image: url('${process.env.PUBLIC_URL}/assets/bg-${props => props.randomNumber}.webp');
+  background-image: url('./bg-${props => props.randomNumber}.webp');
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
